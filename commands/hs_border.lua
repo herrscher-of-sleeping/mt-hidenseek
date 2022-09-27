8820703ae9339f5fa7527964df176dea02fff328
Sayu <herrscher.of.sleeping@protonmail.com>
2022-09-27 21:46:43 +0300

    Refactored /hs_border command for further changes

--- commands/hs_border.lua
+++ commands/hs_border.lua
@@ -66,27 +66,28 @@ end
 local function remove_wall(pos, radius, height)
   for x = pos.x - radius, pos.x + radius do
     for z = pos.z - radius, pos.z + radius do
-      for y = pos.y, pos.y + height do
+      for y = pos.y - height, pos.y + height do
         safe_unset_border_node({ x = x, y = y, z = z })
       end
     end
   end
+  return true
 end
 
-local function command_handler(name, command)
-  local params = HideNSeek.util.split_string(command)
+local subcommands = {}
+
+local function get_rounded_player_position(name)
   local pos = minetest.get_player_by_name(name):get_pos()
   pos.x = math.round(pos.x)
   pos.y = math.round(pos.y)
   pos.z = math.round(pos.z)
-  local subcmd = params[1]
-  if subcmd ~= "circle" and subcmd ~= "rm" then
-    return false, "Unknown subcommand: " .. subcmd
-  end
-  local radius = tonumber(params[2])
-  local height = tonumber(params[3])
-  local is_force = params[4] == "force"
+  return pos
+end
 
+subcommands.circle = function(name, params)
+  local radius = tonumber(params[1])
+  local height = tonumber(params[2])
+  local is_force = params[3] == "force"
   if not radius or radius <= 0 then
     return false, "Radius should be integer > 0"
   end
@@ -102,12 +103,52 @@ local function command_handler(name, command)
     end
   end
 
-  if subcmd == "circle" then
-    add_wall(pos, radius, height)
-  elseif subcmd == "rm" then
-    remove_wall(pos, radius, height)
+  local pos = get_rounded_player_position(name)
+  return add_wall(pos, radius, height)
+end
+
+subcommands.rm = function(name, params)
+  local radius = tonumber(params[1])
+  local height = tonumber(params[2])
+  local is_force = params[3] == "force"
+  if not radius or radius <= 0 then
+    return false, "Radius should be integer > 0"
   end
-  return true
+  if params[2] == "force" then
+    height = radius
+    is_force = true
+  end
+  if not height then height = radius; end
+  if not radius or radius <= 0 then
+    return false, "Radius should be integer > 0"
+  end
+  if height and height <= 0 then
+    return false, "Height should be integer > 0"
+  end
+  if not is_force then
+    if radius > 50 then
+      return false, "Radius shouldn't be > 50"
+    end
+    if height > 50 then
+      return false, "Height shouldn't be > 50"
+    end
+  end
+
+  local pos = get_rounded_player_position(name)
+  return remove_wall(pos, radius, height or radius)
+end
+
+local function command_handler(name, command)
+  local params = HideNSeek.util.split_string(command)
+
+  local subcmd = params[1]
+
+  if not subcommands[subcmd] then
+    return false, "Unknown subcommand: " .. subcmd
+  end
+
+  table.remove(params, 1)
+  return subcommands[subcmd](name, params)
 end
 
 local function init(mod_namespace, is_reload)
